2d2b7132ae3f1a5b226dbba5ce63a74d42a41323
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-02-16 12:48:04 +0100

    Add test method add and subtract in calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator simpleCalculator = new Calculator();
+
+        int resultAdd = simpleCalculator.add(5, 2);
+
+        if (resultAdd > 0 || resultAdd < 0 || resultAdd == 0){
+            System.out.println("test method add OK");
+        } else {
+            System.out.println("Error! method add");
+        }
+
+        int resultSubtract = simpleCalculator.subtract(5, 2);
+
+        if (resultSubtract > 0 || resultSubtract < 0 || resultSubtract == 0){
+            System.out.println("test method subtract OK");
+        } else {
+            System.out.println("Error! method subtract");
+        }
     }
 }
