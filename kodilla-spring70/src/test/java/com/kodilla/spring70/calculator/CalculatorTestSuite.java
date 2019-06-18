8094a5960f04f6a7dc7695af91bbbc785fad2955
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-06-18 23:13:45 +0200

    Edit 14.2

--- kodilla-spring70/src/test/java/com/kodilla/spring70/calculator/CalculatorTestSuite.java
+++ kodilla-spring70/src/test/java/com/kodilla/spring70/calculator/CalculatorTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.spring70.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,26 +12,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CalculatorTestSuite {
+    @Autowired
+     private Calculator calculator;
 
     @Test
     public void testCalculationsAdd() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring70");
-        Calculator calculator  = context.getBean(Calculator.class);
         //When
-        //library.loadFromDb();
-         double addResult =   calculator.add(3,2);
+         double addResult = calculator.add(3,2);
         //Then
         Assert.assertEquals(5, addResult, 0.1);
     }
 
     @Test
     public void testCalculationsSub() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring70");
-        Calculator calculator  = context.getBean(Calculator.class);
         //When
         double subResult =   calculator.sub(3,2);
         //Then
@@ -39,10 +33,6 @@ public class CalculatorTestSuite {
 
     @Test
     public void testCalculationsMul() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring70");
-        Calculator calculator  = context.getBean(Calculator.class);
         //When
         double mulResult =   calculator.mul(3,2);
         //Then
@@ -51,10 +41,6 @@ public class CalculatorTestSuite {
 
     @Test
     public void testCalculationsDiv() {
-        //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring70");
-        Calculator calculator  = context.getBean(Calculator.class);
         //When
         double divResult =   calculator.div(3,2);
         //Then
