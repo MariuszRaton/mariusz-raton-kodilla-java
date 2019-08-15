cb2e8c460f429199534245ec98cadde64a9caf9b
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-08-15 10:06:16 +0200

    Edit 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,6 +4,8 @@ import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -25,9 +27,18 @@ public class StreamMain {
         System.out.println(text4);
 */
    /* 7.3*/
+
+        List<ForumUser> userList = new ArrayList<>();
+        userList.add(new ForumUser(1, "Jan", 'M', 1990, 1, 2, 3));
+        userList.add(new ForumUser(2, "Anna", 'F', 2001, 11, 22, 2));
+        userList.add(new ForumUser(3, "Edward", 'M', 1985, 10, 12, 0));
+        userList.add(new ForumUser(4, "Adam", 'M', 2016, 11, 01, 5));
+        userList.add(new ForumUser(5, "Konrad", 'M', 1985, 04, 24, 2));
+
         Forum forumUsers = new Forum();
         LocalDate now = LocalDate.now();
-        Map<Integer, ForumUser> map = forumUsers.getList().stream()
+
+        Map<Integer, ForumUser> map = forumUsers.getList(userList).stream()
                 .filter(user -> user.getGender() == 'M' )
                 .filter(user -> user.getDateOfBirth().isBefore(now.minusYears(20)))
                 .filter(user -> user.getPostsOfPublished() > 1)
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -6,18 +6,11 @@ public final class Forum {
 
     private final List<ForumUser> userList;
 
-    public Forum() {
+       public Forum() {
         userList = new ArrayList<>();
     }
 
-    public List<ForumUser> getList() {
-
-        userList.add(new ForumUser(1, "Jan", 'M', 1990, 1, 2, 3));
-        userList.add(new ForumUser(2, "Anna", 'F', 2001, 11, 22, 2));
-        userList.add(new ForumUser(3, "Edward", 'M', 1985, 10, 12, 0));
-        userList.add(new ForumUser(4, "Adam", 'M', 2016, 11, 01, 5));
-        userList.add(new ForumUser(5, "Konrad", 'M', 1985, 04, 24, 2));
-
+    public List<ForumUser> getList(List<ForumUser> userList) {
         return userList;
     }
 }
