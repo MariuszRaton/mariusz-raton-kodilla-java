1da4c5426ad30bd95ac12b508b924ffe23043ddc
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-09-03 17:06:48 +0200

    Edit 13.4

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/ShopFood2Door/SaleProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/ShopFood2Door/SaleProcessor.java
@@ -11,8 +11,9 @@ public class SaleProcessor {
         for (Map.Entry<Product, Integer> entry : productList.entrySet()){
             if (entry.getKey().getProductQuantity() < entry.getValue()) {
                 System.out.println("Brak towaru " + entry.getKey().getProductName() + " próbujesz zamówić " + entry.getValue() + "szt Maksymalnie można zamówić " + entry.getKey().getProductQuantity() + "szt");
+            } else {
+                entry.getKey().getProvider().process(entry.getKey());
             }
-            entry.getKey().getProvider().process(entry.getKey());
         }
         return true;
     }
