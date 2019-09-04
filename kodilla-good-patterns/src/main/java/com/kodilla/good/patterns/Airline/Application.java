47da9dd7d0eeefd9b274d0d2befbba3ac5eef6c0
Mariusz Ratoń <raton.mariusz@gmail.com>
2019-09-04 22:08:14 +0200

    Add 13.5

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Airline/Application.java
@@ -0,0 +1,25 @@
+package com.kodilla.good.patterns.Airline;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Map<String, List<String>> flightsMap = new HashMap<>();
+        flightsMap.put("Warszawa", Arrays.asList("Katowice", "Gdańsk"));
+        flightsMap.put("Katowice", Arrays.asList("Praga"));
+        flightsMap.put("Praga", Arrays.asList("Londyn"));
+        flightsMap.put("Londyn", Arrays.asList("Moskwa"));
+
+        FlightProcessor flightProcessor = new FlightProcessor(flightsMap);
+
+        final Map<String, List<String>>  allPossibleFlightFromAirport = flightProcessor.getPossibleFlightFromAirport(flightsMap, "Warszawa");
+
+        final boolean flightPossible = flightProcessor.findFilght("Warszawa", "Moskwa");
+
+        final Map<String, List<String>>  allPossibleFlightToAirport = flightProcessor.getPossibleFlightToAirport(flightsMap, "Praga");
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Airline/FlightProcessor.java
@@ -0,0 +1,66 @@
+package com.kodilla.good.patterns.Airline;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class FlightProcessor {
+
+    private final Map<String, List<String>> flightsMap;
+
+    public FlightProcessor(Map<String, List<String>> flightsMap) {
+        this.flightsMap = flightsMap;
+    }
+
+    public boolean findFilght(String departureAirport, String arrivalAirport) {
+
+        if (!flightsMap.containsKey(departureAirport)) {
+            return false;
+        }
+
+        if (flightsMap.get(departureAirport).contains(arrivalAirport)) {
+            System.out.println("Lot na lotnisko: " + arrivalAirport + " możliwy z przesiadkami: ");
+            return  true;
+        }
+
+        for (String newDepartureAirport : flightsMap.get(departureAirport)) {
+            if (findFilght(newDepartureAirport, arrivalAirport)) {
+                System.out.println("Przesiadka na lotnisku: " + newDepartureAirport);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Map<String, List<String>> getPossibleFlightFromAirport(Map<String, List<String>> flightsMap, String departureAirport) {
+
+        System.out.println("Wszystkie bezpośrednie loty z lotniska " + departureAirport + ": ");
+        flightsMap.get(departureAirport).stream()
+                .forEach(System.out::println);
+
+        System.out.println("");
+        return flightsMap;
+    }
+
+    public Map<String, List<String>> getPossibleFlightToAirport(Map<String, List<String>> flightsMap, String arrivalAirport) {
+
+        System.out.println("\nBezpośredni lot na lotnisko " + arrivalAirport + " możliwy z lotniska: ");
+        flightsMap.get(arrivalAirport).stream()
+                .forEach(System.out::println);
+
+        return flightsMap;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlightProcessor that = (FlightProcessor) o;
+        return flightsMap.equals(that.flightsMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(flightsMap);
+    }
+}
